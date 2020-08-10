aedcc970290e0d50bf3def96b7a0bcd243ca7641
dchicchon <danielchicchon@gmail.com>
2020-08-10 07:29:04 -0700

    reformat root route for heroku

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -2,10 +2,9 @@ const router = require("express").Router();
 
 router.get("/", (req, res) => {
   console.log("Setting API Home Page");
-  let message = ```Welcome to the Leaderboard API! Please navigate to the following paths:
-  1. /elected
-  2. /users```;
-  res.send(message);
+  res.send(
+    "Welcome to the Leaderboard API! Please navigate to the following paths: 1. /elected 2. /users"
+  );
 });
 
 module.exports = router;
