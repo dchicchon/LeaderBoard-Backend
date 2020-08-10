ba199239e0a93aa96865071443afbaf3da17a38f
dchicchon <danielchicchon@gmail.com>
2020-08-10 08:36:58 -0700

    switch to mongoose syntax for queries

--- controllers/electedController.js
+++ controllers/electedController.js
@@ -1,39 +1,55 @@
-const mongoose = require("mongoose");
-const db = mongoose.connection;
-const electedCollection = db.collection("elected-officials");
+const Elected = require("../models/Elected");
+// const mongoose = require("mongoose");
+// const db = mongoose.connection;
+// const electedCollection = db.collection("elected-officials");
 
 module.exports = {
   getAllElected: (req, res) => {
-    electedCollection
-      .find()
-      .toArray()
-      .then((results) => {
-        res.json(results);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  },
-  getOneElected: (req, res) => {
-    console.log({ req });
-    electedCollection
-      .findOne()
+    Elected.find()
       .then((result) => {
         res.json(result);
       })
       .catch((err) => {
         console.error(err);
       });
+    // electedCollection
+    //   .find()
+    //   .toArray()
+    //   .then((results) => {
+    //     res.json(results);
+    //   })
+    //   .catch((err) => {
+    //     console.error(err);
+    //   });
+  },
+
+  getAllElectedFrom: (req, res) => {
+    console.log(req.params);
+    res.send("Returning all elected");
+  },
+
+  getOneElected: (req, res) => {
+    // console.log({ req });
+    Elected.findOne();
+    // electedCollection
+    //   .findOne()
+    //   .then((result) => {
+    //     res.json(result);
+    //   })
+    //   .catch((err) => {
+    //     console.error(err);
+    //   });
   },
   postOneElected: (req, res) => {
     console.log("Add One Elected");
-    electedCollection
-      .insertOne(req.body)
-      .then((result) => {
-        res.json(result);
-      })
-      .catch((err) => {
+    const newElected = new Elected(req.body);
+    newElected.save((err, result) => {
+      if (err) {
         console.error(err);
-      });
+      } else {
+        console.log(result);
+        res.json(result);
+      }
+    });
   },
 };
